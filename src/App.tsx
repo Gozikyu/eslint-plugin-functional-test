7f1ad7d2d88c0d4b8ba35541b49a26ca5486d647
yuta <gozikyu@gmail.com>
2022-11-27 11:28:33 +0900

    eslintを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+type Props = {
+  readonly name: string;
+  readonly hobbies: readonly string[];
+};
+
+const User = (props: Props) => {
+  return (
+    <div>
+      <div>name is {props.name}</div>
+      <h2>hobbies</h2>
+      {props.hobbies.map((hobby) => (
+        <p>{hobby}</p>
+      ))}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User name={"taro"} hobbies={["tennis", "game", "manga"]} />
     </div>
   );
 }
